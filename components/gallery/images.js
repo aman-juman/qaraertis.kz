7d5573f4cfad0b1d4cad894e849b845a88d4c741
Aman Juman <alamanshogun@gmail.com>
2023-01-29 15:29:00 +0600

    version-1.19

--- components/gallery/images.js
+++ components/gallery/images.js
@@ -2,17 +2,6 @@ import { Image } from "react-grid-gallery";
 
 export const images = [
     {
-        src: "/images/img1.jpeg",
-        original: "/images/img1.jpeg",
-        caption: "Кара Ертис - крестьянское хозяйство",
-    },
-    {
-        src: "/images/img2.jpeg",
-        original: "/images/img2.jpeg",
-
-        caption: "Кара Ертис - крестьянское хозяйство",
-    },
-    {
         src: "/images/img3.jpeg",
         original: "/images/img3.jpeg",
         caption: "Кара Ертис - крестьянское хозяйство",
@@ -20,11 +9,6 @@ export const images = [
     {
         src: "/images/img4.jpeg",
         original: "/images/img4.jpeg",
-        caption: "Кара Ертис - крестьянское хозяйство",
-    },
-    {
-        src: "/images/img5.jpeg",
-        original: "/images/img5.jpeg",
         caption: "Кара Ертис - крестьянское хозяйство",
     },
     {
@@ -39,16 +23,6 @@ export const images = [
         caption: "Кара Ертис - крестьянское хозяйство",
     },
     {
-        src: "/images/img8.jpeg",
-        original: "/images/img8.jpeg",
-        caption: "Кара Ертис - крестьянское хозяйство",
-    },
-    {
-        src: "/images/img9.jpeg",
-        original: "/images/img9.jpeg",
-        caption: "Кара Ертис - крестьянское хозяйство",
-    },
-    {
         src: "/images/img10.jpeg",
         original: "/images/img10.jpeg",
 
@@ -60,29 +34,13 @@ export const images = [
         caption: "Кара Ертис - крестьянское хозяйство",
     },
     {
-        src: "/images/img12.jpeg",
-        original: "/images/img12.jpeg",
-        caption: "Кара Ертис - крестьянское хозяйство",
-    },
-    {
         src: "/images/img13.jpeg",
         original: "/images/img13.jpeg",
         caption: "Кара Ертис - крестьянское хозяйство",
     },
     {
-        src: "/images/img14.jpeg",
-        original: "/images/img14.jpeg",
-
-        caption: "Кара Ертис - крестьянское хозяйство",
-    },
-    {
         src: "/images/img15.jpeg",
         original: "/images/img15.jpeg",
-        caption: "Кара Ертис - крестьянское хозяйство",
-    },
-    {
-        src: "/images/img16.jpeg",
-        original: "/images/img16.jpeg",
         caption: "Кара Ертис - крестьянское хозяйство",
     },
     {
@@ -102,24 +60,8 @@ export const images = [
         caption: "Кара Ертис - крестьянское хозяйство",
     },
     {
-        src: "/images/img20.jpeg",
-        original: "/images/img20.jpeg",
-        caption: "Кара Ертис - крестьянское хозяйство",
-    },
-    {
         src: "/images/img21.jpeg",
         original: "/images/img21.jpeg",
-        caption: "Кара Ертис - крестьянское хозяйство",
-    },
-    {
-        src: "/images/img22.jpeg",
-        original: "/images/img22.jpeg",
-
-        caption: "Кара Ертис - крестьянское хозяйство",
-    },
-    {
-        src: "/images/img23.jpeg",
-        original: "/images/img23.jpeg",
         caption: "Кара Ертис - крестьянское хозяйство",
     },
     {
@@ -151,38 +93,6 @@ export const images = [
     {
         src: "/images/img29.jpeg",
         original: "/images/img29.jpeg",
-        caption: "Кара Ертис - крестьянское хозяйство",
-    },
-    {
-        src: "/images/img30.jpeg",
-        original: "/images/img30.jpeg",
-
-        caption: "Кара Ертис - крестьянское хозяйство",
-    },
-    {
-        src: "/images/img31.jpeg",
-        original: "/images/img31.jpeg",
-        caption: "Кара Ертис - крестьянское хозяйство",
-    },
-    {
-        src: "/images/img32.jpeg",
-        original: "/images/img32.jpeg",
-        caption: "Кара Ертис - крестьянское хозяйство",
-    },
-    {
-        src: "/images/img33.jpeg",
-        original: "/images/img33.jpeg",
-        caption: "Кара Ертис - крестьянское хозяйство",
-    },
-    {
-        src: "/images/img34.jpeg",
-        original: "/images/img34.jpeg",
-
-        caption: "Кара Ертис - крестьянское хозяйство",
-    },
-    {
-        src: "/images/img35.jpeg",
-        original: "/images/img35.jpeg",
         caption: "Кара Ертис - крестьянское хозяйство",
     },
     {
