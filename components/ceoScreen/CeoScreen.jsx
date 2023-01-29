195e27a447eba96a3cfcff0deb475ef16ee9f6cb
Aman Juman <alamanshogun@gmail.com>
2023-01-29 17:04:47 +0600

    version-1.25

--- components/ceoScreen/CeoScreen.jsx
+++ components/ceoScreen/CeoScreen.jsx
@@ -3,13 +3,50 @@ import Image from "next/image";
 import img1 from "./img1.jpeg";
 import img2 from "./img2.jpeg";
 import certificate from "./certificate.jpeg";
+import {useContext} from "react";
+import {LanguageContext} from "@/pages";
+
+const dataBase = [
+    {
+        language: "ru",
+        content: [
+            {
+                name: "Карыбаев Женисбек",
+                position: "руководитель Крестьянского Хозяйство"
+            },
+            {
+                name: "Карыбаев Кайыр Женисбекулы",
+                position: "Заместитель руководителя Крестьянского Хозяйство"
+            }
+        ]
+    },
+    {
+        language: "kz",
+        content: [
+            {
+                name: "Қарыбаев Жеңісбек",
+                position: "Шаруа қожалық басшысы"
+            },
+            {
+                name: "Қарыбаев Қайыр Жеңісбекұлы",
+                position: "Шаруа қожалық басшысының орынбасары"
+            }
+        ]
+    }
+]
 const CeoScreen = () => {
+    const [language, setLanguage] = useContext(LanguageContext);
+    const index = dataBase.findIndex(item => item.language === language);
+    const data = dataBase[index].content;
     return (
         <section className={styles.section}>
             <div className="container">
                 <div className={styles.list}>
-                    <Card img={img1} fullName="Карыбаев Женисбек " position="руководитель Крестьянского Хозяйство" />
-                    <Card img={img2} fullName="Карыбаев Кайыр Женисбекулы" position="Заместитель руководителя Крестьянского Хозяйство" />
+                    {
+                        data.map((person, i) =>(
+                            <Card key={i} img={ (i+1 === 1) ? img1 : img2} fullName={person.name} position={person.position} />
+                        ))
+                    }
                 </div>
                 {/*<Image src={certificate}/>*/}
             </div>
