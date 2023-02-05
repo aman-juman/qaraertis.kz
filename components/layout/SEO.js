1645cf56e8889df82a7337e9848d56c2522f092d
Aman Juman <alamanshogun@gmail.com>
2023-02-06 01:18:37 +0600

    version-1.36

--- components/layout/SEO.js
+++ components/layout/SEO.js
@@ -28,7 +28,7 @@ export const SEO = ({title = "Кара Ертис"}) => {
             <link rel="manifest" href="/manifest.json"/>
             <link rel="apple-touch-icon" href="/apple-icon.png"></link>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"  />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link
                 href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Lobster&family=Montserrat+Alternates:wght@400;500;600;700&family=Montserrat:wght@400;500;700&display=swap"
                 rel="stylesheet" />
